840b0ef8df2036e3bb5cd9dc82a4cb1a7219b803
Lucas da Rocha Cantarelli <lucascantarellijpa@gmail.com>
2021-04-27 19:16:04 -0300

    melhorias no script

--- build.sh
+++ build.sh
@@ -1,3 +1,17 @@
 #docker-compose up -d portainer
 
 docker stack deploy --compose-file=docker-compose.yml portainer
+sleep 10
+clear
+echo "#### Portainer - WEB UI"
+echo ""
+echo "Endereço IP:"
+echo -n "http://$(docker node inspect self --format '{{ .Status.Addr  }}'):9000"
+echo ""
+echo ""
+echo "#  Lista de Containers no Stack"
+echo "#---------------------------------------"
+docker ps
+echo ""
+echo ""
+echo ""
